96621b9a827e2c3058d41818cdb7ea173c21b644
Mia Herman <miaherman@Mias-MBP.lan>
2020-11-12 16:30:26 +0100

    fixed divs and resume

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     const content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } 
+      else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
   });
 }
