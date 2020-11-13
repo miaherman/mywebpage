13e78bdfefae17f462bccfa1975f23f9a83a3abe
miaherman <mia.herman@live.se>
2020-11-13 15:14:51 +0100

    finished project

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const coll = document.getElementsByClassName("foldout");
+/** Gets buttonID from HTML */
+const fold = document.getElementsByClassName("foldout");
 let i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
+/** For loop for buttons that fold out when clicked */
+for (i = 0; i < fold.length; i++) {
+  fold [i].addEventListener("click", function () {
     this.classList.toggle("active");
     const content = this.nextElementSibling;
 
@@ -16,8 +18,8 @@ for (i = 0; i < coll.length; i++) {
 }
 
 /** 
-function blooming() {
-  var image = document.getElementById('myImage');
+function imageframe() {
+  let image = document.getElementById('myImage');
   if (image.src.match("lime-blob")) {
     image.src = "/images/pattern.png"
   } else {
